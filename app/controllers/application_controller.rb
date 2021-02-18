61f5af60b49914383df7a3879122c163d7faa81f
asuka <kn7ssaerkv284inpbe18s@gmail.com>
2021-02-18 14:17:00 +0000

    Finish edit-attendances

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,63 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  include SessionsHelper
+class AttendancesController < ApplicationController
+  before_action :set_user, only: [:edit_one_month, :update_one_month]
+  before_action :logged_in_user, only: [:update, :edit_one_month]
+  before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
+  before_action :set_one_month, only: :edit_one_month
 
-  $days_of_the_week = %w{日 月 火 水 木 金 土}
+  UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
 
-  # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
-def set_one_month 
-  @first_day = params[:date].nil? ?
-  Date.current.beginning_of_month : params[:date].to_date
-  @last_day = @first_day.end_of_month
-  one_month = [*@first_day..@last_day]
-
-  @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
-
-  unless one_month.count == @attendances.count
-    ActiveRecord::Base.transaction do
-      one_month.each { |day| @user.attendances.create!(worked_on: day) }
+  def update
+    @user = User.find(params[:user_id])
+    @attendance = Attendance.find(params[:id])
+    # 出勤時間が未登録であることを判定します。
+    if @attendance.started_at.nil?
+      if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
+        flash[:info] = "おはようございます！"
+      else
+        flash[:danger] = UPDATE_ERROR_MSG
+      end
+    elsif @attendance.finished_at.nil?
+      if @attendance.update_attributes(finished_at: Time.current.change(sec: 0))
+        flash[:info] = "お疲れ様でした。"
+      else
+        flash[:danger] = UPDATE_ERROR_MSG
+      end
     end
-    @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
+    redirect_to @user
   end
 
-rescue ActiveRecord::RecordInvalid
-  flash[:danger] = "ページ情報の取得に失敗しました、再アクセスしてください。"
-  redirect_to root_url
-end
+  def edit_one_month
+  end
+
+  def update_one_month
+    ActiveRecord::Base.transaction do # トランザクションを開始します。
+      attendances_params.each do |id, item|
+        attendance = Attendance.find(id)
+        attendance.update_attributes!(item)
+      end
+    end
+    flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+    redirect_to user_url(date: params[:date])
+  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
+    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+    redirect_to attendances_edit_one_month_user_url(date: params[:date])
+  end
+
+  private
+
+    # 1ヶ月分の勤怠情報を扱います。
+    def attendances_params
+      params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
+    end
+
+    # beforeフィルター
+
+    # 管理権限者、または現在ログインしているユーザーを許可します。
+    def admin_or_correct_user
+      @user = User.find(params[:user_id]) if @user.blank?
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger] = "編集権限がありません。"
+        redirect_to(root_url)
+      end  
+    end
 end
